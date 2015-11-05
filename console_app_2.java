8628e4af193307ee613b0629e0121e24714a2dc0
devashish-rawat <dev.rawat7396@gmail.com>
2015-11-05 09:28:09 +0530

    Added Search Filters : Year , Male/Female.
    Displays top 10 Names according to Filters Applied.
    Can't Display for Both male/female.

---
+++ console_app_2.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class console_app_2 
+{
+	static Scanner userInput = new Scanner (System.in);
+	
+	public static void main(String[] args)
+	{
+		int i=1;
+		
+		
+		String fileName;
+		
+		String year , gender ;
+		
+		System.out.println("Enter male / female");
+		gender=userInput.nextLine();
+		
+		System.out.println("Enter year");
+		year=userInput.nextLine();
+		
+		fileName = gender + "_cy" + year + "_top" + ".csv";
+		//System.out.println(fileName);
+		
+		File file = new File (fileName);
+		
+		try
+		{
+			Scanner inputStream = new Scanner (file);
+			
+			inputStream.next(); //to remove Column Headers
+			inputStream.next();
+			
+			while(i<=10)
+			{
+				String data = inputStream.next();
+				String []values = data.split(",");
+				System.out.println(values[0]);
+				i++;
+			}
+			inputStream.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+}
